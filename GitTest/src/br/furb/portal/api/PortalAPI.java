3d97c202fcea810416428bcb6ffd2ded8f3174ea
Ana Paula Pandini <ana.pandini@gmail.com>
2012-03-30 20:23:23 -0300

    Implementação para desenhar vários outros triângulos representanto o frustum redimensionado pelo portal.

--- GitTest/src/br/furb/portal/api/PortalAPI.java
+++ GitTest/src/br/furb/portal/api/PortalAPI.java
@@ -8,8 +8,11 @@ import java.util.Map;
 import javax.microedition.khronos.opengles.GL10;
 
 import br.furb.portal.api.model.Camera;
+import br.furb.portal.api.model.Divisao;
 import br.furb.portal.api.model.Frustum;
+import br.furb.portal.api.model.Ponto;
 import br.furb.portal.api.model.Sala;
+import br.furb.portal.api.model.TipoDivisao;
 import br.furb.portal.api.model.WayPoint;
 
 public class PortalAPI {
@@ -42,35 +45,76 @@ public class PortalAPI {
 		// TODO aqui está sendo usado o índice 1 pois é o frustum "original". Pode ser estudada uma forma melhor de fazer,
 		// ou apenas documentar o motivo de estar implementado desta forma
 		camera.mover(frustum.get(1).getAngulo());
-
-		// para cada portal do ambiente da câmera
-		// verificar se o frustum passa pelo portal
-		// duas formas de fazer:
-		// - ver se o portal intercepta as três linhas do frustum
-		// - ou ver se os pontos do portal estão contidos no frustum
-		// se o frustum passa pelo portal
-		// reajustar o portal
-
 	}
 
 	public void atualizar() {
 		camera.atualizar();
 		// TODO arruma os frustums conforme paredes / portais
-		// for (Divisao div : camera.getSala().getDivisoes()) {
-		// for (int i = 1; i <= indiceFrustum; i++) {
-		// Frustum f = frustum.get(i);
-		// if (PortalAPI_Utils.intesercta(f.getFrustumOrigin(), f.getFrustumLeft(), div.getDestino(), div.getOrigem())) {
-		// if (div.getTipo() == TipoDivisao.PORTAL) {
-		//
-		// } else {
-		// // TipoDivisao.PAREDE
-		// // TODO aqui deve diminuir ele para ver apenas estes pontos?
-		// }
-		// }
-		// }
-		// }
+		Frustum f = frustum.get(1); // Pega o frustum original para quebrar a visão conforme o ambiente
+		f.atualizar();
+		indiceFrustum = 1;
+		for (Divisao div : camera.getSala().getDivisoes()) {
+			Ponto pr = null, pl = null;
+			if (PortalAPI_Utils.intersecta(f.getFrustumOrigin(), f.getFrustumLeft(), div.getDestino(), div.getOrigem())) {
+				if (div.getTipo() == TipoDivisao.PORTAL) {
+					pl = f.getFrustumLeft();
+					if (PortalAPI_Utils.canReach(f, div.getDestino())) {
+						pr = PortalAPI_Utils.getInterseccaoRetas(f.getFrustumOrigin(), div.getDestino(), f.getFrustumLeft(), f.getFrustumRight());
+					} else if (PortalAPI_Utils.canReach(f, div.getOrigem())) {
+						pr = PortalAPI_Utils.getInterseccaoRetas(f.getFrustumOrigin(), div.getOrigem(), f.getFrustumLeft(), f.getFrustumRight());
+					} else {
+						pr = f.getFrustumRight();
+					}
+				} else {
+					// Ponto de intersecção do lado esquerdo do frustum com esta parede
+					pl = PortalAPI_Utils.getInterseccaoRetas(f.getFrustumOrigin(), f.getFrustumLeft(), div.getDestino(), div.getOrigem());
+					// Agora, encontra o ponto do outro lado do novo frustum
+					if (PortalAPI_Utils.canReach(f, div.getDestino())) {
+						pr = div.getDestino();
+					} else if (PortalAPI_Utils.canReach(f, div.getOrigem())) {
+						pr = div.getOrigem();
+					} else {
+						if (PortalAPI_Utils.intersecta(f.getFrustumOrigin(), f.getFrustumRight(), div.getDestino(), div.getOrigem())) {
+							pr = PortalAPI_Utils.getInterseccaoRetas(f.getFrustumOrigin(), f.getFrustumRight(), div.getDestino(), div.getOrigem());
+						} else {
+							pr = f.getFrustumRight();
+						}
+					}
+				}
+			} else if (PortalAPI_Utils.intersecta(f.getFrustumOrigin(), f.getFrustumRight(), div.getDestino(), div.getOrigem())) {
+				if (div.getTipo() == TipoDivisao.PORTAL) {
+					pr = f.getFrustumRight();
+					if (PortalAPI_Utils.canReach(f, div.getDestino())) {
+						pl = PortalAPI_Utils.getInterseccaoRetas(f.getFrustumOrigin(), div.getDestino(), f.getFrustumLeft(), f.getFrustumRight());
+					} else if (PortalAPI_Utils.canReach(f, div.getOrigem())) {
+						pl = PortalAPI_Utils.getInterseccaoRetas(f.getFrustumOrigin(), div.getOrigem(), f.getFrustumLeft(), f.getFrustumRight());
+					} else {
+						pl = f.getFrustumLeft();
+					}
+				} else {
+					// Ponto de intersecção do lado esquerdo do frustum com esta parede
+					pr = PortalAPI_Utils.getInterseccaoRetas(f.getFrustumOrigin(), f.getFrustumRight(), div.getDestino(), div.getOrigem());
+					// Agora, encontra o ponto do outro lado do novo frustum
+					if (PortalAPI_Utils.canReach(f, div.getDestino())) {
+						pl = div.getDestino();
+					} else if (PortalAPI_Utils.canReach(f, div.getOrigem())) {
+						pl = div.getOrigem();
+					} else {
+						if (PortalAPI_Utils.intersecta(f.getFrustumOrigin(), f.getFrustumLeft(), div.getDestino(), div.getOrigem())) {
+							pl = PortalAPI_Utils.getInterseccaoRetas(f.getFrustumOrigin(), f.getFrustumLeft(), div.getDestino(), div.getOrigem());
+						} else {
+							pl = f.getFrustumLeft();
+						}
+					}
+				}
+			}
+			if (pr != null && pl != null) {
+				Frustum novoFrustum = new Frustum(f.getFrustumOrigin(), pr, pl, gl);
+				frustum.put(++indiceFrustum, novoFrustum);
+			}
+		}
 		// TODO atualiza os frustums
-		for (int i = 1; i <= indiceFrustum; i++) {
+		for (int i = 2; i <= indiceFrustum; i++) {
 			frustum.get(i).atualizar();
 		}
 		// TODO fim atualização dos frustums
@@ -81,7 +125,7 @@ public class PortalAPI {
 			// TODO aqui pode ser utilizada uma abordagem que não necessite ainda fazer um loop sobre todos os waypoints, e fazer loop só nos da sala primeiro
 			if (wp.getSala().getIdentificadorSala() == camera.getSala().getIdentificadorSala()) {
 				for (int i = 1; i <= indiceFrustum; i++) {
-					boolean r = frustum.get(i).canReach(wp);
+					boolean r = PortalAPI_Utils.canReach(frustum.get(i), wp);
 					wp.setReached(r);
 				}
 			}
