f21e0e6dd1771dff518f635ed21518c06c0b9976
Ana Paula Pandini <ana.pandini@gmail.com>
2012-04-17 23:37:00 -0300

    Javadoc.

--- GitTest/src/br/furb/portal/api/PortalAPI.java
+++ GitTest/src/br/furb/portal/api/PortalAPI.java
@@ -13,9 +13,31 @@ import br.furb.portal.api.model.PontoInteresse;
 import br.furb.portal.api.model.Sala;
 import br.furb.portal.api.model.TipoDivisao;
 
+/**
+ * Trabalho de Conclusão de Curso II
+ * Fundação Universidade Regional de Blumenau - FURB
+ * Orientador: Dalton Solano dos Reis
+ * Biblioteca de algoritmos de portais para a plataforma Android
+ * 
+ * @author Ana Paula Pandini
+ */
 public class PortalAPI {
 
-	// TODO nome do método temporário, avaliar nome melhor
+	/**
+	 * Identifica os pontos de interesse que estão no campo de visão do observador. <br>
+	 * Inicia testando apenas os pontos que estão na mesma sala em que o observador está. Após isso,
+	 * detecta se dentro do campo de visão há algum portal. Se houver, explora o trecho sala que pode ser visto por esse portal.
+	 * E faz essas verificações recursivamente por todos os portais de todas as salas que são alcançadas pelo campo de visão original do observador.
+	 * 
+	 * @param fo
+	 * @param fd
+	 * @param fe
+	 * @param salaCorrente
+	 * @param pontosInteresse
+	 * @param camera
+	 * @param idSalasVisitadas
+	 * @param frustumsAuxiliares
+	 */
 	private void visao(Ponto fo, Ponto fd, Ponto fe, Sala salaCorrente, List<PontoInteresse> pontosInteresse, Camera camera, List<Integer> idSalasVisitadas, List<Frustum> frustumsAuxiliares) {
 		if (!idSalasVisitadas.contains(salaCorrente.getIdentificadorSala())) {
 
@@ -31,8 +53,6 @@ public class PortalAPI {
 
 			Ponto novoFd = null;
 			Ponto novoFe = null;
-			// TODO se eu quiser depois desenhar todos os frustum, preciso armazenar estar coordenadas descobertas em uma lista
-			// para que a API devolva para o usuário essas informações
 			for (Divisao div : salaCorrente.getPortais()) {
 				if (PortalAPI_Utils.intersecta(fo, fe, div.getDestino(), div.getOrigem())) {
 					novoFe = fe;
@@ -54,7 +74,7 @@ public class PortalAPI {
 					}
 				}
 
-				// se tiver as coordenadas preenchidas, significa que tem um novo frustum para a próxima sala
+				// Se tiver as coordenadas preenchidas, significa que tem um novo frustum para a próxima sala
 				if (novoFe != null && novoFd != null) {
 					frustumsAuxiliares.add(new Frustum(fo, novoFe, novoFd));
 					visao(fo, novoFd, novoFe, retornaProximaSala(salaCorrente, div), pontosInteresse, camera, idSalasVisitadas, frustumsAuxiliares);
@@ -63,6 +83,13 @@ public class PortalAPI {
 		}
 	}
 
+	/**
+	 * Retorna a sala que pode ser vista pelo portal passado por parâmetro.
+	 * 
+	 * @param salaCorrente
+	 * @param div
+	 * @return
+	 */
 	private Sala retornaProximaSala(Sala salaCorrente, Divisao div) {
 		if (salaCorrente == div.getSalaDestino()) {
 			return div.getSalaOrigem();
@@ -70,20 +97,37 @@ public class PortalAPI {
 		return div.getSalaDestino();
 	}
 
+	/**
+	 * @see br.furb.portal.api.PortalAPI#visao(br.furb.portal.api.model.Ponto, br.furb.portal.api.model.Ponto, br.furb.portal.api.model.Ponto, br.furb.portal.api.model.Sala, java.util.List, br.furb.portal.api.model.Camera, java.util.List, java.util.List)
+	 */
 	public List<Frustum> visaoCamera(List<PontoInteresse> pontosInteresse, Map<Integer, Sala> salas, Camera camera, Frustum frustum) {
 		List<Frustum> frustumsAuxiliares = new ArrayList<Frustum>();
 		visao(frustum.getFrustumOrigin(), frustum.getFrustumRight(), frustum.getFrustumLeft(), camera.getSala(), pontosInteresse, camera, new ArrayList<Integer>(), frustumsAuxiliares);
 		return frustumsAuxiliares;
 	}
 
+	/**
+	 * /**
+	 * Calcula as novas coordenadas do observador, verificando se ele pode movimentar-se ou não. <br>
+	 * Quando houver uma parede no caminho, este método irá detectar e não irá atualizar as coordenadas do observador, para que ele não se mova. <br>
+	 * Caso contrário, irá movimentar o observador, verificar se ele trocou de sala e também identificar os pontos de interesse que estão
+	 * no campo de visão da nova posição.
+	 * 
+	 * @param camera
+	 * @param novoXCamera
+	 * @param novoYCamera
+	 * @param pontosInteresse
+	 * @param salas
+	 * @param frustum
+	 */
 	public void moverCamera(Camera camera, float novoXCamera, float novoYCamera, List<PontoInteresse> pontosInteresse, Map<Integer, Sala> salas, Frustum frustum) {
 		boolean podeMover = true;
 		for (Divisao div : camera.getSala().getDivisoes()) {
-			// TODO necessário documentar este trecho de código
+
 			if (PortalAPI_Utils.intersecta(camera, new Ponto(novoXCamera, novoYCamera, null), div.getOrigem(), div.getDestino())) {
 				if (div.getTipo() == TipoDivisao.PORTAL) {
 					if (div.getSalaOrigem().getIdentificadorSala() == camera.getSala().getIdentificadorSala()) {
-						camera.setSala(div.getSalaDestino()); // TODO já seta aqui ou apenas quando a pessoa decidir REALMENTE mudar a câmera?
+						camera.setSala(div.getSalaDestino());
 					} else {
 						camera.setSala(div.getSalaOrigem());
 					}
--- GitTest/src/br/furb/portal/api/model/Divisao.java
+++ GitTest/src/br/furb/portal/api/model/Divisao.java
@@ -4,6 +4,14 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
+/**
+ * Trabalho de Conclusão de Curso II
+ * Fundação Universidade Regional de Blumenau - FURB
+ * Orientador: Dalton Solano dos Reis
+ * Biblioteca de algoritmos de portais para a plataforma Android
+ * 
+ * @author Ana Paula Pandini
+ */
 public class Divisao {
 
 	private Ponto origem;
@@ -11,15 +19,30 @@ public class Divisao {
 	private TipoDivisao tipo;
 	private FloatBuffer coordenadasDivisao;
 
+	/**
+	 * Atributo <code>salaOrigem</code> utilizado apenas quando a Divisão é do tipo <code>TipoDivisao.PORTAL</code>.
+	 */
 	private Sala salaOrigem;
 	private Sala salaDestino;
 
+	/**
+	 * Inicializa a divisão, informando já o seu tipo.
+	 * 
+	 * @param origem
+	 * @param destino
+	 * @param tipo
+	 */
 	public Divisao(Ponto origem, Ponto destino, TipoDivisao tipo) {
 		this.origem = origem;
 		this.destino = destino;
 		this.tipo = tipo;
 	}
 
+	/**
+	 * Retorna as coordenadas para desenho da divisão.
+	 * 
+	 * @return
+	 */
 	public FloatBuffer getBufferCoordenadas() {
 		if (coordenadasDivisao == null) {
 			float divCoords[] = new float[] { getOrigem().getX(), getOrigem().getY(), 0, getDestino().getX(), getDestino().getY(), 0 };
@@ -32,30 +55,65 @@ public class Divisao {
 		return coordenadasDivisao;
 	}
 
+	/**
+	 * Retorna um dos pontos da divisão.
+	 * 
+	 * @return
+	 */
 	public Ponto getOrigem() {
 		return origem;
 	}
 
+	/**
+	 * Retorna o ponto de destino da divisão.
+	 * 
+	 * @return
+	 */
 	public Ponto getDestino() {
 		return destino;
 	}
 
+	/**
+	 * Retorna o tipo da divisão: portal / parede.
+	 * 
+	 * @return
+	 */
 	public TipoDivisao getTipo() {
 		return tipo;
 	}
 
+	/**
+	 * Retorna a sala de origem da divisão caso ela for do tipo <code>TipoDivisao.PORTAL</code>.
+	 * 
+	 * @return
+	 */
 	public Sala getSalaOrigem() {
 		return salaOrigem;
 	}
 
+	/**
+	 * Grava a sala de origem da divisão quando ela é do tipo <code>TipoDivisao.PORTAL</code>.
+	 * 
+	 * @param salaOrigem
+	 */
 	public void setSalaOrigem(Sala salaOrigem) {
 		this.salaOrigem = salaOrigem;
 	}
 
+	/**
+	 * Retorna a sala de destino da divisão caso ela for do tipo <code>TipoDivisao.PORTAL</code>.
+	 * 
+	 * @return
+	 */
 	public Sala getSalaDestino() {
 		return salaDestino;
 	}
 
+	/**
+	 * Grava a sala de destino da divisão quando ela é do tipo <code>TipoDivisao.PORTAL</code>.
+	 * 
+	 * @param salaDestino
+	 */
 	public void setSalaDestino(Sala salaDestino) {
 		this.salaDestino = salaDestino;
 	}
