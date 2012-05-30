4a04e75851c6b774dbe26587bf19978aae1d8b95
Ana Paula Pandini <ana.pandini@gmail.com>
2012-05-30 00:51:35 -0300

    Testes do ambiente 03.

--- GitTest/src/br/furb/portais/aplicacao/Controle.java
+++ GitTest/src/br/furb/portais/aplicacao/Controle.java
@@ -47,7 +47,7 @@ public class Controle {
 	public Controle(GL10 gl) {
 		this.gl = gl;
 		AmbientesUtil ambienteUtil = new AmbientesUtil();
-		ambienteUtil.ambienteTeste01();
+		ambienteUtil.ambienteTeste02();
 		this.salas = ambienteUtil.getSalas();
 		this.pontosInteresse = ambienteUtil.getPontosInteresse();
 		this.anguloVisao = 180.0f;
